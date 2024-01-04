ea9166cb2da4a55b056a551a838d2c10712bfcc8
RuinTwraith <dibbenduk@gmail.com>
2024-01-04 20:21:52 +0530

    Updated loader styles

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -39,11 +39,11 @@ export default function Loader(props) {
 
       {/* loader - 3 - start */}
       <div className="loader-4 flex gap-1">
-        <div className="block block-1 w-10 h-4 bg-rose-300 relative rounded" />
-        <div className="block block-2 w-10 h-4 bg-rose-400 relative rounded" />
-        <div className="block block-3 w-10 h-4 bg-rose-500 relative rounded" />
-        <div className="block block-4 w-10 h-4 bg-rose-600 relative rounded" />
-        <div className="block block-5 w-10 h-4 bg-rose-700 relative rounded" />
+        <div className="block block-1 w-10 h-4 bg-orange-300 relative rounded" />
+        <div className="block block-2 w-10 h-4 bg-orange-400 relative rounded" />
+        <div className="block block-3 w-10 h-4 bg-orange-500 relative rounded" />
+        <div className="block block-4 w-10 h-4 bg-orange-600 relative rounded" />
+        <div className="block block-5 w-10 h-4 bg-orange-700 relative rounded" />
       </div>
       {/* loader - 3 - end */}
     </div>
