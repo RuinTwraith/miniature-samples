dc5c663cf02a362f3628f54f2689891a9ec8b21a
RuinTwraith <dibbenduk@gmail.com>
2024-01-05 16:24:42 +0530

    Cleanup in loader and tab1

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,7 +2,7 @@ import "./style.css";
 
 export default function Loader(props) {
   return (
-    <div className="loader-container flex flex-col gap-8">
+    <div className="loader-container flex flex-col gap-16 ml-10">
       <button className="flex gap-4 w-40 h-12 items-center border rounded-lg px-3 hover:scale-105 transition-transform">
         {/* loader starts */}
         <div
@@ -14,31 +14,40 @@ export default function Loader(props) {
       </button>
 
       {/* loader - 2 - start */}
-      <div className="loader-2 flex">
+      <div className="loader loader-2 flex">
         <div className="loader-2__left w-20 h-20 bg-teal-700 rounded-full" />
         <div className="loader-2__right w-20 h-20 rounded-full backdrop-blur-md" />
       </div>
       {/* loader - 2 - end */}
 
       {/* loader - 3 - start */}
-      <div className="loader-3 flex flex-col">
-        <div className="block block-1 w-20 h-4 bg-rose-300 relative rounded" />
-        <div className="block block-2 w-20 h-4 bg-rose-400 relative rounded" />
-        <div className="block block-3 w-20 h-4 bg-rose-500 relative rounded" />
-        <div className="block block-4 w-20 h-4 bg-rose-600 relative rounded" />
-        <div className="block block-5 w-20 h-4 bg-rose-700 relative rounded" />
+      <div className="loader loader-3 flex flex-col">
+        <div className="block block-1 w-20 h-3 bg-rose-300 relative rounded" />
+        <div className="block block-2 w-20 h-3 bg-rose-400 relative rounded" />
+        <div className="block block-3 w-20 h-3 bg-rose-500 relative rounded" />
+        {/* <div className="block block-4 w-20 h-4 bg-rose-600 relative rounded" />
+        <div className="block block-5 w-20 h-4 bg-rose-700 relative rounded" /> */}
       </div>
       {/* loader - 3 - end */}
 
-      {/* loader - 3 - start */}
-      <div className="loader-4 flex gap-1">
+      {/* loader - 4 - start */}
+      <div className="loader loader-4 flex gap-1">
         <div className="block block-1 w-10 h-4 bg-orange-300 relative rounded" />
         <div className="block block-2 w-10 h-4 bg-orange-400 relative rounded" />
         <div className="block block-3 w-10 h-4 bg-orange-500 relative rounded" />
         <div className="block block-4 w-10 h-4 bg-orange-600 relative rounded" />
         <div className="block block-5 w-10 h-4 bg-orange-700 relative rounded" />
       </div>
-      {/* loader - 3 - end */}
+      {/* loader - 4 - end */}
+
+      {/* loader - 5 - start */}
+      <div className="loader loader-5 flex gap-1 items-center border rounded-lg py-2 px-4 w-min">
+        <div className="block block-1 w-2 h-2 bg-slate-600 relative rounded-full" />
+        <div className="block block-2 w-2 h-2 bg-slate-500 relative rounded-full" />
+        <div className="block block-3 w-2 h-2 bg-slate-400 relative rounded-full" />
+        <p className="font-bold text-xl ml-4">Typing</p>
+      </div>
+      {/* loader - 5 - end */}
     </div>
   );
 }
--- src/components/Tab1/Tab1.jsx
+++ src/components/Tab1/Tab1.jsx
@@ -17,40 +17,54 @@ export default function Tab1(props) {
   };
 
   const init = async function () {
+    // creating a promise and resolving or rejecting it based on condition
     const promise = new Promise((resolve, reject) => {
       setTimeout(() => {
         if (condition) {
           reject("Error");
         }
-        resolve("dadsasd");
+        resolve("Message");
       }, 1000);
     });
     promise
-      .finally((res) => {
-        console.log("Finally 1");
-        updateConsole("Finally 1");
+      .finally(() => {
+        // always get called in the beginning
+        console.log("Finally - 1");
+        updateConsole("Finally - 1");
       })
       .then((res) => {
-        console.log("Promise resolved 1", res);
-        updateConsole("Promise resolved 1", res);
-        return "from then 1";
+        // called when the promise resolves
+        console.log("Then - 1", res);
+        updateConsole("Then - 1", res);
+        return "Updated Message";
       })
       .catch((err) => {
+        // called only when error is thrown
         console.error("Catch - 1", err);
         updateConsole("Catch - 1", err);
+        return "Updated Error";
       })
       .then((res) => {
+        // always get called even after error
         console.log("Then - 2", res);
         updateConsole("Then - 2", res);
       })
-      .finally((res) => {
+      .finally(() => {
+        // always get called in the end
         console.log("Finally - 2");
         updateConsole("Finally - 2");
       });
   };
 
   return (
-    <div className="w-full flex flex-col items-center justify-center mt-20">
+    <div className="w-full flex flex-col items-center justify-center mt-10">
+      <p className="mb-2">
+        Example for promises and try, catch, finally. Change if the promise will
+        reject or resolve by clicking on <b>'Throw error'</b> button.
+      </p>
+      <p className="mb-10">
+        Click <b>'Press Me!'</b> to get consoles
+      </p>
       <Button onClick={handleClick}>Press Me!</Button>
       <Button
         onClick={() => setCondition((prev) => !prev)}
